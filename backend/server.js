7311f955529291cc94a119303e6b2a7cb199b789
Bhavik_Kathiriya <bhavikkathiriya1428@gmail.com>
2026-03-15 16:50:01 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'https://smarthealthcaresystem.vercel.app',
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
